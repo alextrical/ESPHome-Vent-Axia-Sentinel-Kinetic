3dc3884dbd99a206d20546e141c0c2723d2d8737
alextrical <35117191+alextrical@users.noreply.github.com>
2025-06-02 12:39:10 +0100

    remove parse flags

--- components/opentherm/hub.cpp
+++ components/opentherm/hub.cpp
@@ -8,31 +8,6 @@ namespace opentherm {
 
 static const char *const TAG = "opentherm";
 namespace message_data {
-bool parse_flag8_lb_0(OpenthermData &data) { return read_bit(data.valueLB, 0); }
-bool parse_flag8_lb_1(OpenthermData &data) { return read_bit(data.valueLB, 1); }
-bool parse_flag8_lb_2(OpenthermData &data) { return read_bit(data.valueLB, 2); }
-bool parse_flag8_lb_3(OpenthermData &data) { return read_bit(data.valueLB, 3); }
-bool parse_flag8_lb_4(OpenthermData &data) { return read_bit(data.valueLB, 4); }
-bool parse_flag8_lb_5(OpenthermData &data) { return read_bit(data.valueLB, 5); }
-bool parse_flag8_lb_6(OpenthermData &data) { return read_bit(data.valueLB, 6); }
-bool parse_flag8_lb_7(OpenthermData &data) { return read_bit(data.valueLB, 7); }
-bool parse_flag8_hb_0(OpenthermData &data) { return read_bit(data.valueHB, 0); }
-bool parse_flag8_hb_1(OpenthermData &data) { return read_bit(data.valueHB, 1); }
-bool parse_flag8_hb_2(OpenthermData &data) { return read_bit(data.valueHB, 2); }
-bool parse_flag8_hb_3(OpenthermData &data) { return read_bit(data.valueHB, 3); }
-bool parse_flag8_hb_4(OpenthermData &data) { return read_bit(data.valueHB, 4); }
-bool parse_flag8_hb_5(OpenthermData &data) { return read_bit(data.valueHB, 5); }
-bool parse_flag8_hb_6(OpenthermData &data) { return read_bit(data.valueHB, 6); }
-bool parse_flag8_hb_7(OpenthermData &data) { return read_bit(data.valueHB, 7); }
-uint8_t parse_u8_lb(OpenthermData &data) { return data.valueLB; }
-uint8_t parse_u8_hb(OpenthermData &data) { return data.valueHB; }
-int8_t parse_s8_lb(OpenthermData &data) { return (int8_t) data.valueLB; }
-int8_t parse_s8_hb(OpenthermData &data) { return (int8_t) data.valueHB; }
-uint16_t parse_u16(OpenthermData &data) { return data.u16(); }
-uint16_t parse_u8_lb_60(OpenthermData &data) { return data.valueLB * 60; }
-uint16_t parse_u8_hb_60(OpenthermData &data) { return data.valueHB * 60; }
-int16_t parse_s16(OpenthermData &data) { return data.s16(); }
-float parse_f88(OpenthermData &data) { return data.f88(); }
 
 void write_flag8_lb_0(const bool value, OpenthermData &data) { data.valueLB = write_bit(data.valueLB, 0, value); }
 void write_flag8_lb_1(const bool value, OpenthermData &data) { data.valueLB = write_bit(data.valueLB, 1, value); }
